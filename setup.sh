8c8f653811492149017890921a264d9f70d9dcd7
LiLGun-X <91993374+LiLGun-X@users.noreply.github.com>
2022-07-02 20:33:47 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -107,7 +107,7 @@ echo "        ╭━━━━━━━━━━━━━━━━━━━━━
         ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯   "
     read -p "        ╰━━ ( Y/n ) : " -e -i y Confirn
     if [[ "$Confirn" = "y" || "$Confirn" = "Y" ]]; then
-wget -q -O install "https://raw.githubusercontent.com/Lil-Gun-X/HYPER-X-SCRIPT/main/Install/install"
+wget -q -O install "https://github.com/LiLGun-X/HYPER-X-SCRIPT/blob/main/%C9%AA%C9%B4s%E1%B4%9B%E1%B4%80%CA%9F%CA%9F/install"
 bash install
 exit
 elif [[ "$Confirn" = "n" || "$Confirn" = "N" ]]; then
